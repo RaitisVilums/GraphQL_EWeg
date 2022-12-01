efd1069d69a497388df1771ea17ae2d87abcac92
RaitisVilums <rvilums80@gmail.com>
2022-12-01 14:31:53 +0200

    starting to use REDUCERS

--- src/contexts/user.cotext.jsx
+++ src/contexts/user.cotext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useReducer } from "react";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
@@ -8,9 +8,31 @@ export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
 });
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+const INITIAL_STATE = {
+  currentUser: null,
+};
 
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispach] = useReducer(userReducer, INITIAL_STATE);
+
   const value = {
     currentUser,
     setCurrentUser,
